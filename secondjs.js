5efb91b23424089ba05cba4cccb19f9bcbf09f11
comafj <51936362+comafj@users.noreply.github.com>
2020-06-02 17:20:24 +0900

    First commit with overall layout

---
+++ secondjs.js
@@ -0,0 +1,8 @@
+// This allows the Javascript code inside this block to only run when the page
+// has finished loading in the browser.
+$( document ).ready(function() {
+  var btn = document.getElementById("CBTN");
+  btn.onclick = function(){
+    location.href="first.html";
+  };
+});

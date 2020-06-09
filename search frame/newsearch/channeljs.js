a221391a7ddb4514a9bb0af938f3f7034a5d0bec
comafj <cokrafj@gmail.com>
2020-06-09 10:01:15 +0900

    Search frame update

--- search frame/newsearch/channeljs.js
+++ search frame/newsearch/channeljs.js
@@ -1,43 +1,14 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $( document ).ready(function() {
-  var btn = document.getElementById("CBTN");
-  btn.onclick = function(){
-    var option_val = $("#select_option option:selected").val();
-    var input_var = document.getElementById("input_query");
-    input_var.value="";
-
-    if(option_val=="all"){
-      console.log(option_val);
-    }
-    else if(option_val=="channel"){
-      console.log(option_val);
-    }
-    else if(option_val=="song"){
-      console.log(option_val);
-    }
-    else if(option_val=="singer"){
-      console.log(option_val);
-    }
-    else if(option_val=="creator"){
-      console.log(option_val);
-    }
-    else{
-      console.log("error");
-    }
-  };
-  $("#input_query").keydown(function(event) {
-    if (event.keyCode == 13)
-      document.getElementById("CBTN").click();
-  });
   var div = document.getElementsByClassName("Rsingle");
   div[0].onclick = function(){
     var vID = 'SrVV73gTBpk';
-    location.href="https://www.youtube.com/watch?v=SrVV73gTBpk";
+    location.href="https://www.youtube.com/watch?v=EqW_It2aPH4";
   };
   div[1].onclick = function(){
     var vID = 'EqW_It2aPH4';
-    location.href="https://www.youtube.com/watch?v=EqW_It2aPH4";
+    location.href="https://www.youtube.com/watch?v=SrVV73gTBpk";
   };
   div[2].onclick = function(){
     var vID = 'd9HdIy0MM0U';
--- search frame/newsearch/songjs.js
+++ search frame/newsearch/songjs.js
@@ -4,14 +4,14 @@ $( document ).ready(function() {
   var div = document.getElementsByClassName("Rsingle");
   div[0].onclick = function(){
     var vID = 'SrVV73gTBpk';
-    location.href="https://www.youtube.com/watch?v=SrVV73gTBpk";
+    location.href="https://www.youtube.com/watch?v=kDGc8zZ9HhY";
   };
   div[1].onclick = function(){
     var vID = 'EqW_It2aPH4';
-    location.href="https://www.youtube.com/watch?v=TXYST-k0p_0";
+    location.href="https://www.youtube.com/watch?v=l6IxCYp-pw4";
   };
   div[2].onclick = function(){
     var vID = 'd9HdIy0MM0U';
-    location.href="https://www.youtube.com/watch?v=ElGCb7o41tU";
+    location.href="https://www.youtube.com/watch?v=xeg31UhG3DY";
   };
 });

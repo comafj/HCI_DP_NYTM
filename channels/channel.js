93066ae7be22296f45f6cb5fdd97dee197e60188
BeomsikPark <korkor727@naver.com>
2020-06-20 21:19:03 +0900

    Fix minor bugs

--- channels/channel.js
+++ channels/channel.js
@@ -84,9 +84,11 @@ async function isJoined(b){
   }
   else{
     console.log("insert JoinedChannel");
-    firebase.database().ref(`/${username}/JoinedChannel/`).push({videocode: videocode,
+    if(!b){
+      firebase.database().ref(`/${username}/JoinedChannel/`).push({videocode: videocode,
                                                               title: title,
                                                               action: "EnterChannel"});
+      }
     var videoBtn = document.getElementById("like");
     videoBtn.innerHTML=`<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>`;
     return true;
